bce651d9950e5ff25a8f83f9eab517ae5e3d89e7
dragoon8192_mac <dragoon8192@gmail.com>
2021-03-09 00:08:52 +0900

    add install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh -e
+
+IGNORE_PATTERN="^\.(git|travis)"
+
+echo "Create dotfile links..."
+for dotfile in .??*; do
+    [[ $dotfile =~ $IGNORE_PATTERN ]] && continue
+    if [ -e "$HOME/$dotfile" ] ;then
+        echo "Back up $HOME/$dotfile to $HOME/$dotfile~"
+        mv "$HOME/$dotfile" "$HOME/$dotfile~"
+    fi
+    ln -sniv "$(pwd)/$dotfile" "$HOME/$dotfile"
+done
+echo "Success!!"
