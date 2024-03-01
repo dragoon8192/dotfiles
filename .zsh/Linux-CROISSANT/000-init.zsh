8920e97fc99f54789f0aefda335ddb86a16875e6
Dragoon8192 <dragoon8192@gmail.com>
2024-03-01 18:25:33 +0900

    update:   .zsh/Linux-CROISSANT/000-init.zsh
    Use `ip route` and assign HOST_IPV4.

--- .zsh/Linux-CROISSANT/000-init.zsh
+++ .zsh/Linux-CROISSANT/000-init.zsh
@@ -13,11 +13,12 @@ export HOMEBREW_CELLAR=$(brew --prefix)"/Cellar";
 export HOMEBREW_REPOSITORY=$(brew --prefix)"/Homebrew";
 
 # #ポートフォワーディング
-WSL_IP=$(ip addr show eth0 | grep 'inet ' | grep -E '[0-9]{,3}\.[0-9]{,3}\.[0-9]{,3}\.[0-9]{,3}' -o | sed -n '1p')
+WSL2_IPV4=$(ip route | grep -Po '(?<=eth0 proto kernel scope link src )(\d{1,3}\.){3}\d{1,3}')
+HOST_IPV4=0.0.0.0
 LISTEN_PORTS=(22 1334 8000)
 PORT_SHIFT=50000
 for port in $LISTEN_PORTS ; do
     netsh.exe interface portproxy delete v4tov4 listenport=$(($port + $PORT_SHIFT)) 1> /dev/null
-    netsh.exe interface portproxy add    v4tov4 listenport=$(($port + $PORT_SHIFT)) connectaddress=$WSL_IP connectport=$port 1> /dev/null
+    netsh.exe interface portproxy add    v4tov4 listenaddress=$HOST_IPV4 listenport=$(($port + $PORT_SHIFT)) connectaddress=$WSL2_IPV4 connectport=$port 1> /dev/null
 done
 netsh.exe interface portproxy show all
