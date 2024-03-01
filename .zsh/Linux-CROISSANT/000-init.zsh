4128648ce27ee453a665df5bd96473e92291bf9a
Dragoon8192 <dragoon8192@gmail.com>
2024-03-01 16:54:30 +0900

    update:   .zsh/Linux-CROISSANT/000-init.zsh
    Port-forwarding.

--- .zsh/Linux-CROISSANT/000-init.zsh
+++ .zsh/Linux-CROISSANT/000-init.zsh
@@ -1,19 +1,23 @@
-#minttyのカラー設定
+# #minttyのカラー設定
 source ~/etc/mintty-colors-solarized/sol.light
-#X11
+
+# #X11
 #export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0.0
 #export LIBGL_ALWAYS_INDIRECT=yes
 #xauth
 #xauth generate $DISPLAY
-#homebrew
+
+# #HOMEBREW
 export HOMEBREW_PREFIX=$(brew --prefix)
 export HOMEBREW_CELLAR=$(brew --prefix)"/Cellar";
 export HOMEBREW_REPOSITORY=$(brew --prefix)"/Homebrew";
 
-#ssh接続用のポートフォワーディング
-IP=$(ip addr show eth0 | grep 'inet ' | grep -E '[0-9]{,3}\.[0-9]{,3}\.[0-9]{,3}\.[0-9]{,3}' -o | sed -n '1p')
-netsh.exe interface portproxy delete v4tov4 listenport=31522
-netsh.exe interface portproxy add    v4tov4 listenport=31522 connectaddress=$IP connectport=22
-# sc.exe config iphlpsvc start=auto
-# sc.exe start  iphlpsvc
-# sudo service ssh start
+# #ポートフォワーディング
+WSL_IP=$(ip addr show eth0 | grep 'inet ' | grep -E '[0-9]{,3}\.[0-9]{,3}\.[0-9]{,3}\.[0-9]{,3}' -o | sed -n '1p')
+LISTEN_PORTS=(22 1334 8000)
+PORT_SHIFT=50000
+for port in $LISTEN_PORTS ; do
+    netsh.exe interface portproxy delete v4tov4 listenport=$(($port + $PORT_SHIFT)) 1> /dev/null
+    netsh.exe interface portproxy add    v4tov4 listenport=$(($port + $PORT_SHIFT)) connectaddress=$WSL_IP connectport=$port 1> /dev/null
+done
+netsh.exe interface portproxy show all
