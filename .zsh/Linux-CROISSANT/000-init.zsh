22306b7c4f77ae16c10379a48c1cda66fe63464f
Dragoon8192 <dragoon8192@gmail.com>
2024-03-09 00:19:24 +0900

    update:   .zsh/Linux-CROISSANT/000-init.zsh
    Port forwarding 7860:57860

--- .zsh/Linux-CROISSANT/000-init.zsh
+++ .zsh/Linux-CROISSANT/000-init.zsh
@@ -15,7 +15,7 @@ export HOMEBREW_REPOSITORY=$(brew --prefix)"/Homebrew";
 # #ポートフォワーディング
 WSL2_IPV4=$(ip route | grep -Po '(?<=eth0 proto kernel scope link src )(\d{1,3}\.){3}\d{1,3}')
 HOST_IPV4=0.0.0.0
-LISTEN_PORTS=(22 1334 8000)
+LISTEN_PORTS=(22 1334 7860 8000)
 PORT_SHIFT=50000
 for port in $LISTEN_PORTS ; do
     netsh.exe interface portproxy delete v4tov4 listenport=$(($port + $PORT_SHIFT)) 1> /dev/null
