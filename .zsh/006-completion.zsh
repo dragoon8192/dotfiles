83a8d523c8564e006126bf17b73b5d98cd0b20cc
Dragoon8192 <dragoon8192@gmail.com>
2025-01-13 12:05:04 +0900

    update:   GitHub CLI の補完に対応.

--- .zsh/006-completion.zsh
+++ .zsh/006-completion.zsh
@@ -1,11 +1,18 @@
-##補完
+# # 補完
 autoload -Uz compinit && compinit
 autoload -Uz bashcompinit && bashcompinit
 zstyle ':completion:*' menu select
 setopt CORRECT
+# # ツールごとの補完
+# ## stack
 if [[ -x "stack" ]];then
     eval "$(stack --bash-completion-script stack)"
 fi
+# ## wp-completion.bash
 if [[ -f ~/etc/wp-completion.bash ]];then
     source ~/etc/wp-completion.bash
+fi
+# ## GitHub CLI
+if [[ -x "gh" ]];then
+    eval "$(gh completion -s zsh)"
 fi
